7878119e38297a7e3f9daba875f776b8f82ceb93
Siddhi Gavhane <siddhigavhane03@gmail.com>
2025-12-16 08:47:15 +0530

    updated file

---
+++ Fruits/index.js
@@ -0,0 +1,6 @@
+const apple = require("./apple");
+const banana = require("./banana");
+const Jackfruit = require("./jackFriut");
+
+let fruit_value = [apple,banana,Jackfruit];
+ module.exports = fruit_value;
